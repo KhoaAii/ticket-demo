b5ed42881338477120d13ae6d84d59c221c5b17f
Trần Đăng Khoa <khoatdse172986@fpt.edu.vn>
2024-05-15 21:41:04 +0700

    git init

---
+++ src/main/java/com/example/ticketsystem/entity/SuatChieu.java
@@ -0,0 +1,39 @@
+package com.example.ticketsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.UUID;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "suatchieu")
+@Accessors(chain = true)
+public class SuatChieu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcType(VarcharJdbcType.class)
+    private UUID id;
+
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "gio_bat_dau")
+    private LocalTime gioBatDau;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Film> film;
+}
